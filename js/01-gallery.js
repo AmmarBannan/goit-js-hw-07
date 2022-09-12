4e7ef2e2bb8d363e24ec08fe02bf43628f60e2c5
Ammar Bannan <ammarabannan@gmail.com>
2022-09-12 23:46:53 +0300

    first try

---
+++ js/01-gallery.js
@@ -0,0 +1,70 @@
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+gallery.innerHTML= galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                    </a>
+                </div>`;
+      })
+      .join('');
+gallery.addEventListener('click', openInstanceModal);
+
+let galleryIndex;
+
+function openInstanceModal(e) {
+    e.preventDefault();
+
+    console.log(e)
+    const selected = (element) => element.description == e.target.alt;
+    galleryIndex=galleryItems.findIndex(selected);
+
+    instance.element().querySelector('img').src = e.target.dataset.source;
+    instance.show();
+}
+
+const instance = basicLightbox.create(
+  `
+      <img src="" />
+  `,
+  {
+    onShow: () => {
+      console.log('start');
+      document.addEventListener('keydown', escBtnHandler);
+    },
+    onClose: () => {
+      console.log('exit');
+      document.removeEventListener('keydown', escBtnHandler);
+    },
+  }
+);
+
+function escBtnHandler(e) {
+  if (e.key === 'Escape') {
+    console.log("dsds");
+    instance.close();
+  }
+  else if (e.keyCode === 37 || e.keyCode === 38) {
+    if(galleryIndex>0){
+        console.log("next",galleryIndex-=1);
+        instance.element().querySelector('img').src = galleryItems[galleryIndex].original;
+    }
+  }
+  else if (e.keyCode === 39 || e.keyCode === 40) {
+
+    if(galleryIndex<galleryItems.length-1){
+        console.log("back",galleryIndex+=1);
+        instance.element().querySelector('img').src = galleryItems[galleryIndex].original;
+    }
+  }
+//   else if (e.keyCode === '39') {
+//     instance.close();
+//   }
+}
+
